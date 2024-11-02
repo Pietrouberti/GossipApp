a25ad95f3e305dbfd97d7570ad77c1359bcc2163
PietroUberti <pietrouberti2003@gmail.com>
2024-11-02 21:14:09 +0000

    Login Page added

--- gossip_frontend/src/views/LoginView.vue
+++ gossip_frontend/src/views/LoginView.vue
@@ -1,4 +1,8 @@
 <template>
+    <div class="login">
+        <input type="text" class="login__input">
+        <input type="text" class="login__input">
+    </div>
 </template>
 <script setup>
 </script>
