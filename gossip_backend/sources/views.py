53b402e724df19e2d68a64f5743d3ce5e098ec7c
olalha <oahague@gmail.com>
2024-11-03 05:10:02 +0000

    CreateMessageView

--- gossip_backend/sources/views.py
+++ gossip_backend/sources/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework import status, views
-from .utils import create_discussion
+from .utils import receive_msg
 
 # Create your views here.
 
@@ -14,8 +14,6 @@ class LoginView(views.APIView):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
-        
-        print(f"{username} - {password}")
 
         if user is not None:
             # Generate or get the token for the authenticated user
@@ -24,22 +22,42 @@ class LoginView(views.APIView):
         else:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-# Create new molecule endpoint
-class CreateDiscussionView(views.APIView):
-    
+# Create new message endpoint
+class CreateMessageView(views.APIView):
+
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    
     def post(self, request):
-        # Extract title and description from the incoming JSON payload
-        text = request.data.get('text')
         
+        author_id = request.user.id
         
-        # Call the store_data function
-        if create_discussion(text):
-            # If storing data is successful, return a success status
-            return Response({'status': 'success'}, status=status.HTTP_200_OK)
+        if 'discussion' in request.data:
+            
+            discussion_id = request.data.get('discussion')
+            text = request.data.get('text')
+            
+            if receive_msg(text=text, author_id=author_id, discussion_id=discussion_id):
+                return Response({'status': 'success'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'status': 'error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
-            # If there was an error (in future logic), return an error status
-            return Response({'status': 'error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            # TODO: Manage priority
+            priority = request.data.get('priority')
+            collaborators = request.data.get('collaborators')
+            text = request.data.get('text')
+            
+            if receive_msg(text=text, author_id=author_id, collaborators=collaborators):
+                return Response({'status': 'success'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'status': 'error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class GetDiscussionsView(views.APIView):
+    pass
+    """
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def request(self, request):
+    """
+        
